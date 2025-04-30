e0108587227eb93d2bc1dd7966cc8d91b78aaf17
ejiro osiephri <osiephriejiro765@gmail.com>
2025-04-30 22:10:54 +0100

    Feats/add added some changes

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,9 @@ const app_module_1 = require("./app.module");
 const passport = require("passport");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({
+        origin: '*',
+    });
     app.use(passport.initialize());
     await app.listen(3000);
 }
